2802efac5b084dc88c3f962245cf67ebc38c4511
Daniel Gorin <daniel.gorin@habito.com>
2018-12-13 16:59:51 +0000

    Fix usage of fetchTarball without sha256
    In `nix/default.nix`, `fetchTarball` was being used to download
    a fixed version of nixpkgs. However, no `sha256` value was given,
    which means, according to the internet [1], that the fetched tarball
    is cached for 1 hour only and then dowloaded again.
    
    Instead of adding the `sha256` value, we use `fetchGit`, since then
    we only need to know the commit hash, which makes it easier for people
    to use when they want to bump nixpkgs. The downside is that the first
    time the repo has to be fetched, it will be a bit slower.
    
    [1] https://github.com/NixOS/nix/issues/2343

--- nixpkgs/default.nix
+++ nixpkgs/default.nix
@@ -1,4 +1,5 @@
-import (fetchTarball {
-   # Nixpkgs checkout from 2018-11-03.
-   url = "https://github.com/NixOS/nixpkgs/archive/01389f1ad6ecb894d2f20f390d944b1e70674a9e.tar.gz";
+import (builtins.fetchGit {
+   name = "nixpkgs-2018-11-03";
+   url = https://github.com/NixOS/nixpkgs/;
+   rev = "01389f1ad6ecb894d2f20f390d944b1e70674a9e";
 })
